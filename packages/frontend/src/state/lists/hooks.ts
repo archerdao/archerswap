4fe73b3ba1f840fe80ee73322fc8a1717a26a0f6
Pyython <vogonpoetry06@gmail.com>
2021-04-20 22:13:09 +0700

    update default list

--- packages/frontend/src/state/lists/hooks.ts
+++ packages/frontend/src/state/lists/hooks.ts
@@ -1,5 +1,5 @@
 import { UNSUPPORTED_LIST_URLS } from './../../constants/lists'
-import DEFAULT_TOKEN_LIST from '@uniswap/default-token-list'
+import DEFAULT_TOKEN_LIST from '../../constants/list.json'
 import { ChainId, Token } from '@archerswap/sdk'
 import { Tags, TokenInfo, TokenList } from '@uniswap/token-lists'
 import { useMemo } from 'react'

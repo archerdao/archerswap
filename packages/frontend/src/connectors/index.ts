8d4a44993a8ece1be0c2720db03dd916eb03c75c
Pyython <vogonpoetry06@gmail.com>
2021-04-20 21:50:03 +0700

    update uniswap references

--- packages/frontend/src/connectors/index.ts
+++ packages/frontend/src/connectors/index.ts
@@ -6,7 +6,7 @@ import { PortisConnector } from '@web3-react/portis-connector'
 
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
-import UNISWAP_LOGO_URL from '../assets/images/logo.png'
+import ARCHERSWAP_LOGO_URL from '../assets/images/logo.png'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
@@ -55,6 +55,6 @@ export const portis = new PortisConnector({
 // mainnet only
 export const walletlink = new WalletLinkConnector({
   url: NETWORK_URL,
-  appName: 'Uniswap',
-  appLogoUrl: UNISWAP_LOGO_URL
+  appName: 'Archerswap',
+  appLogoUrl: ARCHERSWAP_LOGO_URL
 })
--- packages/frontend/src/constants/lists.ts
+++ packages/frontend/src/constants/lists.ts
@@ -14,6 +14,7 @@ const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const KLEROS_LIST = 't2crtokens.eth'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 const BA_LIST = 'https://raw.githubusercontent.com/The-Blockchain-Association/sec-notice-list/master/ba-sec-list.json'
+const ARCHERSWAP_LIST = 'https://storageapi.fleek.co/archerdao-team-bucket/list.json'
 
 export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
 
@@ -32,8 +33,9 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   CMC_STABLECOIN,
   KLEROS_LIST,
   GEMINI_LIST,
+  ARCHERSWAP_LIST,
   ...UNSUPPORTED_LIST_URLS // need to load unsupported tokens as well
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [GEMINI_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [ARCHERSWAP_LIST]

c1e02d7d2d2db2ea3d68909d8116a1610cc08879
Pyython <vogonpoetry06@gmail.com>
2021-06-08 13:02:43 +0700

    disabled coingecko list

--- packages/frontend/src/constants/lists.ts
+++ packages/frontend/src/constants/lists.ts
@@ -7,7 +7,7 @@ const SYNTHETIX_LIST = 'synths.snx.eth'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
 const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
-const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
+// const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const CMC_ALL_LIST = 'defi.cmc.eth'
 const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const KLEROS_LIST = 't2crtokens.eth'
@@ -26,7 +26,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   WRAPPED_LIST,
   OPYN_LIST,
   ROLL_LIST,
-  COINGECKO_LIST,
+  // COINGECKO_LIST,
   CMC_ALL_LIST,
   CMC_STABLECOIN,
   KLEROS_LIST,
